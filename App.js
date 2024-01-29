8f5859bb668ecd403c3b9fd15ca4864df5189dd6
jenraii <131862365+jenraii@users.noreply.github.com>
2024-01-29 14:06:31 +0200

    Update App.js

--- App.js
+++ App.js
@@ -21,16 +21,16 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <Text style={{fontsize: 20}}>Result: {result.toFixed(2)}</Text>
+      <Text style={{fontSize: 20}}>Result: {result.toFixed(2)}</Text>
       <TextInput
       keyboardType="numeric"
-      style={{fontsize: 18, width: 200, borderColor: 'gray', borderWidth: 1}}
+      style={{fontSize: 18, width: 200, borderColor: 'gray', borderWidth: 1}}
       onChangeText={numA => setNumA(numA)}
       value={numA}
       />
       <TextInput
       keyboardType="numeric"
-      style={{fontsize: 18, width: 200, borderColor: 'gray', borderWidth: 1}}
+      style={{fontSize: 18, width: 200, borderColor: 'gray', borderWidth: 1}}
       onChangeText={numB => setNumB(numB)}
       value={numB}
       />
@@ -45,8 +45,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    marginTop: 100,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-end',
   },
+  buttoncontainer: {
+    flex: 2,
+    width: 150,
+    flexDirection: 'row',
+    backgroundColor: '#fff',
+    alignItems: 'flex-start',
+    justifyContent: 'space-around',
+    padding: 20,
+  }
 });
